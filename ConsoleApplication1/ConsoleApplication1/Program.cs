328394aea3b6e67986686af319bae8845cfefcb1
Pretty33 <321838579@qq.com>
2018-11-04 09:04:38 +0800

    DB frist 的实体框架

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,15 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            33
+            //使用数据上下文进行数据操作，using表示上下文代码的范围，执行完后内存会自动释放
+            using (var context = new CourseContext())
+            {
+                //.where .orderby .tolist() 注意调用的顺序
+                var departments = context.Departments.Where(n => n.Name.Contains("工程")).OrderBy(n => n.SortCode).ToList();
+                foreach (var d in departments)
+                    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dsen);
+                Console.ReadKey();
+            }
         }
     }
 }
